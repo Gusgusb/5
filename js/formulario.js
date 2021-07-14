e04250ce307bd782e9881c41c15207b342952048
AniLau <agoluj1990@gmail.com>
2021-07-14 17:21:02 -0500

    feat: form to confirm attendance

---
+++ js/formulario.js
@@ -0,0 +1,33 @@
+function isMobile() {
+    if (sessionStorage.desktop)
+        return false;
+    else if (localStorage.mobile)
+        return true;
+    var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+    for (var i in mobile)
+        if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+    return false;
+}
+
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '+524421247689';
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+    buttonSubmit.disabled = true;
+    setTimeout(() => {
+        let nombre = document.querySelector('#nombre').value;
+        let numero = document.querySelector('#numero').value;
+        let mensaje = 'send?phone=' + telefono + '&text=Hola, ' + nombre + ' confirma asistencia a la Boda de Lily y Jorge con ' + numero + ' acompañante(s).';
+        if(isMobile()) {
+            window.open(urlMobile + mensaje, '_blank');
+        }else{
+            window.open(urlDesktop + mensaje, '_blank');
+        }
+        buttonSubmit.innerHTML = 'Atenderé'
+        buttonSubmit.disabled = false
+    }, 3000);
+});
