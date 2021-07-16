fe044200a0957904725d991a58661d93b9aa5d84
AniLau <agoluj1990@gmail.com>
2021-07-15 21:50:23 -0500

    feat: final touches

--- js/formulario.js
+++ js/formulario.js
@@ -13,7 +13,7 @@ const formulario = document.querySelector('#formulario');
 const buttonSubmit = document.querySelector('#submit');
 const urlDesktop = 'https://web.whatsapp.com/';
 const urlMobile = 'whatsapp://';
-const telefono = '+524421247689';
+const telefono = '+524423628382';
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault();
