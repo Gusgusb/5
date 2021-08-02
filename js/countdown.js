fc48126478d1b43b068f6b9e4977bde833c01eda
Gusgusb <88106085+Gusgusb@users.noreply.github.com>
2021-08-01 21:54:45 -0500

    Add files via upload

--- js/countdown.js
+++ js/countdown.js
@@ -33,4 +33,4 @@ const countdown = (deadline, elem, finalMessage) => {
     }, 1000);
 };
 
-countdown('Oct 9 2021 11:00:00 GMT-0500', 'countdown', "Es hoy!!!");
+countdown('Aug 21 2021 16:00:00 GMT-0500', 'countdown', "Es hoy!!!");
